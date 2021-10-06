d0ac191cb5495e62a52918963b4ef9e11f45f628
Martin Slater <mslater@darkstorm.com.au>
2021-10-06 15:47:54 +1100

    fix: be less explicit about the python3 version to use

--- setup_env.sh
+++ setup_env.sh
@@ -8,7 +8,7 @@ SCRIPT_PATH=`dirname "$BASH_SOURCE"`
 if [ ! -d .pyenv ]; then
 	echo "Creating virtual environment"	
 	pip3 install -q virtualenv
-	virtualenv --python=python3.7 .pyenv
+	virtualenv --python=python3 .pyenv
 fi
 echo "Configuring virtual environment"	
 source ${SCRIPT_PATH}/.pyenv/bin/activate
